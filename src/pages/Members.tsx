399c0201e1750b438c4f79a57ae4327464ae1039
Lrezendecomz <162452619+Lrezendecomz@users.noreply.github.com>
2025-02-12 21:55:01 -0300

    Update Members.tsx

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import BackButton from "../components/BackButton"; // Corrigi o caminho da importação
 
 const members = [
   {
@@ -18,7 +17,6 @@ const members = [
 export default function Members() {
   return (
     <div className="space-y-8">
-      <BackButton /> {/* Adicionado o botão aqui */}
       <h1 className="text-3xl font-bold">Nossos Membros</h1>
       
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
